953d76070161d9dfb1c6b9f84ab679a7c8d68d06
do-ko <doko@onet.eu>
2024-12-04 18:52:40 +0100

    database chat related structure

---
+++ petpals-backend/src/main/java/com/app/petpals/entity/ChatMessage.java
@@ -0,0 +1,27 @@
+package com.app.petpals.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "chat_message")
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "sent_at")
+    private LocalDateTime sentAt;
+
+    @ManyToOne()
+    @JoinColumn(name = "chat_id", nullable = false)
+    private Chatroom chatroom;
+
+    @ManyToOne()
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+}
---
+++ petpals-backend/src/main/java/com/app/petpals/entity/Chatroom.java
@@ -0,0 +1,32 @@
+package com.app.petpals.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "chatroom")
+@Data
+public class Chatroom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "chat_users",
+            joinColumns = @JoinColumn(name = "chat_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "chatroom", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ChatMessage> messages;
+}
